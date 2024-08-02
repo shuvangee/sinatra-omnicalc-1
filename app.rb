c3d08c278fdf3496d223047477852e6ca750149d
Shuvangee Seth <144741662+shuvangee@users.noreply.github.com>
2024-08-02 11:45:45 +0000

    commit 1

--- app.rb
+++ app.rb
@@ -2,8 +2,15 @@ require "sinatra"
 require "sinatra/reloader"
 
 get("/") do
-  "
-  <h1>Welcome to your Sinatra App!</h1>
-  <p>Define some routes in app.rb</p>
-  "
+  erb(:howdy)
+end
+
+get("/square/new") do
+  erb(:square)
+end
+
+get("/square/results") do
+  @the_num= params.fetch("number")
+  @the_result=params.fetch("number").to_f**2
+  erb(:square_results)
 end
